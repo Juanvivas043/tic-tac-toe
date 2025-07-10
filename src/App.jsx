7224ea599f42d4720e3c8397e6608f8671e11a97
Juan Vivas <juanvivas043@gmail.com>
2025-07-09 21:42:40 -0400

    segunda parte del video

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import { useState } from 'react'
 import confetti from 'canvas-confetti'
 import { Square } from './components/Square.jsx'
 import { TURNS } from './constants.js'
-import { WINNER_COMBOS } from './constants.js'
+import { checkWinner } from './logic/board.js'
+import { checkEndGame } from './logic/board.js'
 import './App.css'
+import { WinnerModal } from './components/WinnerModal.jsx'
 export default App
 
 function App() {  
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
   
   const updateBoard = (index) => {
     if (board[index] || winner) return
@@ -26,6 +32,8 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
@@ -41,24 +49,15 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('board')
+
   }
 
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck [c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    return null
-  }
   return (
     <main className='board'>
-      <h1>Tic Tac Toe</h1>
+      <h1>Guau Miau Woof</h1>
       <button onClick={resetGame}>Reset del juego</button>
       <section className='game'>
         {
@@ -85,28 +84,10 @@ function App() {
         </Square>
       </section>
 
-      {
-        winner !== null && (
-          <section className='winner'>
-              <div className='text'>
-                  <h2>
-                    {
-                      winner === false ? 'Empate' : 'Ganó'
-                    }
-                  </h2>
-
-                  <header className='win'>
-                    
-                    {winner && <Square>{winner}</Square>}
-                  </header>
-
-                  <footer>
-                    <button onClick={resetGame}>Empezar de nuevo</button>
-                  </footer>
-              </div>
-          </section>
-        )
-      }
+      <WinnerModal
+        winner={winner}
+        resetGame={resetGame}
+      ></WinnerModal> 
     </main>
   )
 }
